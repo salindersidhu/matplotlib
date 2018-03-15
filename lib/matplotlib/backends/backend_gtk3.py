18d1d82b891196154cca0828d871ce9ec4dd8b8f
Bar Slutsky <bslutsky@gmail.com>
2018-03-15 15:24:44 -0400

    added a condition for showing new window at front
    for backend gtk3

--- lib/matplotlib/backends/backend_gtk3.py
+++ lib/matplotlib/backends/backend_gtk3.py
@@ -413,7 +413,10 @@ class FigureManagerGTK3(FigureManagerBase):
     def show(self):
         # show the figure window
         self.window.show()
-        self.window.present()
+        # if the rcparameter figure.front is set to True, show the new figure
+        # window at the front
+        if rcParams['figure.front']:
+            self.window.present()
 
     def full_screen_toggle(self):
         self._full_screen_flag = not self._full_screen_flag
